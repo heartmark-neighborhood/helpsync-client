ef376aaf0190a3d667bcb22592a759671f65e0d3
sansan2341 <130728163+sansan2341@users.noreply.github.com>
2025-07-03 11:05:47 +0900

    Update and rename SupotterSettingScreen.kt to SupporterSettingScreen.kt

--- app/src/main/java/com/example/helpsync/supotter_setting_screen/SupporterSettingScreen.kt
+++ app/src/main/java/com/example/helpsync/supotter_setting_screen/SupporterSettingScreen.kt
@@ -1,4 +1,4 @@
-package com.example.helpsync.supotter_setting_screen
+package com.example.helpsync.supporter_setting_screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
