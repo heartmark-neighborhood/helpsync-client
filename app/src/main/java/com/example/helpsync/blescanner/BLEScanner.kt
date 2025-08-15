999ad0314b14172800e0f5feb7acbebbf60649da
haru210 <miroku.pr@outlook.jp>
2025-08-15 20:04:52 +0900

    add background BLEScan feature

--- app/src/main/java/com/example/helpsync/blescanner/BLEScanner.kt
+++ app/src/main/java/com/example/helpsync/blescanner/BLEScanner.kt
@@ -1,6 +1,11 @@
 package com.example.helpsync.blescanner
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.app.Service.START_STICKY
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
@@ -8,36 +13,73 @@ import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.os.ParcelUuid
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import java.util.UUID
 
-class BLEScanner(private val context: Context, private val uuid_string: String) {
-    private val serviceUuid = UUID.fromString(uuid_string)
-    private val scanner: BluetoothLeScanner by lazy {
-        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter.bluetoothLeScanner
-    }
+class BLEScanner() : Service() {
+    private val SCAN_DURATION_MS = 18_000L
+    private val CHANNEL_ID = "ble_scan_channel"
+    private lateinit var scanner : BluetoothLeScanner
+    private var handler: Handler? = null
 
     private var scanCallback: ScanCallback? = null
 
-    fun startScan(onMessageReceived: (String) -> Unit) {
-        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN)
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        scanner = bluetoothManager.adapter.bluetoothLeScanner
+        handler = Handler(Looper.getMainLooper())
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val serviceUuid = UUID.fromString(intent?.getStringExtra("UUID"))
+        startForeground(1, createNotification("bluetooth scanning"))
+        startScan(serviceUuid)
+        return START_STICKY
+    }
+    fun startScan(serviceUuid: UUID) {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
             != PackageManager.PERMISSION_GRANTED
         ) {
             Log.w("BLE", "permission denied")
             return
         }
-
+        val context = this;
         scanCallback = object : ScanCallback() {
             override fun onScanResult(callbackType: Int, result: ScanResult) {
                 val data = result.scanRecord?.getServiceData(ParcelUuid(serviceUuid))
                 val msg = data?.toString(Charsets.UTF_8)
+                val intent = Intent("com.example.SCAN_RESULT")
                 if(msg != null) {
-                    onMessageReceived(msg)
+                    intent.setPackage(context.packageName)
+                    intent.putExtra("result", true)
+                    sendBroadcast(intent)
                 }
+                else
+                {
+                    intent.setPackage(context.packageName)
+                    intent.putExtra("result", false)
+                    sendBroadcast(intent)
+                }
+            }
+
+            override fun onScanFailed(errorCode: Int) {
+                val intent = Intent("com.example.SCAN_RESULT")
+                intent.setPackage(context.packageName)
+                intent.putExtra("result", false)
+                sendBroadcast(intent)
             }
         }
 
@@ -49,15 +91,54 @@ class BLEScanner(private val context: Context, private val uuid_string: String) 
             .build()
 
         scanner.startScan(listOf(filter), settings, scanCallback)
+
+        handler?.postDelayed({
+            scanner.stopScan(scanCallback)
+            val intent = Intent("com.example.SCAN_RESULT")
+            intent.setPackage(context.packageName)
+            intent.putExtra("result", false)
+            sendBroadcast(intent)
+        }, SCAN_DURATION_MS)
     }
 
     fun stopScan() {
         scanCallback?.let {
-            if(ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN)
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
                 == PackageManager.PERMISSION_GRANTED
             ) {
                 scanner.stopScan(it)
             }
         }
+    }
+
+    override fun onDestroy() {
+        if(checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED)
+        {
+            scanner.stopScan(scanCallback)
+        }
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    private fun createNotification(text: String): Notification {
+        Log.d("debug", "notif")
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("ヘルプ検索中")
+            .setContentText("付近のヘルプ要請が無いか検索しています")
+            .setSmallIcon(android.R.drawable.stat_sys_data_bluetooth)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID,
+            "HelpSync",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(serviceChannel)
     }
 }
