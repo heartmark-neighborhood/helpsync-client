bec3edcd0fda119dd8f59304f3b57cc98cb0db38
haru210 <miroku.pr@outlook.jp>
2025-10-19 23:37:55 +0900

    fead: make CloudMessageRepository

--- app/src/main/java/com/example/helpsync/repository/CloudMessageRepository.kt
+++ app/src/main/java/com/example/helpsync/repository/CloudMessageRepository.kt
@@ -1,11 +1,47 @@
 package com.example.helpsync.repository
 
+import android.util.Log
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 
 interface CloudMessageRepository {
     val bleRequestMessageFlow: SharedFlow<Map<String, String>>
     val helpRequestMessageFlow: SharedFlow<Map<String, String>>
-    val helpRequestSuccessedMessageFlow: SharedFlow<Map<String, String>>
 
     fun postCloudMessage(data: Map<String, String>)
+}
+
+class CloutMessageRepositoryImpl : CloudMessageRepository {
+
+    private val _bleRequestMessageFlow = MutableSharedFlow<Map<String, String>>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    override val bleRequestMessageFlow: SharedFlow<Map<String, String>> = _bleRequestMessageFlow
+
+    private val _helpRequestMessageFlow = MutableSharedFlow<Map<String, String>>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    override val helpRequestMessageFlow: SharedFlow<Map<String,String>> = _helpRequestMessageFlow
+
+    override fun postCloudMessage(data: Map<String, String>)
+    {
+        when (data["type"]) {
+            "help-request" -> {
+                val success = _helpRequestMessageFlow.tryEmit(data)
+                if(!success) {
+                    Log.w("helpRequestFCM", "Failed to emit helpRequestFCM data")
+                }
+            }
+            "proximity-verification" -> {
+                val success = _bleRequestMessageFlow.tryEmit(data)
+                if(!success) {
+                    Log.w("proximity-verificationFCM", "Failed to emit proximity-verificationFCM data")
+                }
+            }
+
+        }
+    }
 }
