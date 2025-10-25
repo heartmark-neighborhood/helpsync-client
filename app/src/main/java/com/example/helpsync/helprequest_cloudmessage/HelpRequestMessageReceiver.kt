4996332f1603d9481c049334919dd6c1d7af9dc2
haru210 <miroku.pr@outlook.jp>
2025-10-25 23:35:41 +0900

    feat: make onNewToken in HelpRequestMessageReceiver

--- app/src/main/java/com/example/helpsync/helprequest_cloudmessage/HelpRequestMessageReceiver.kt
+++ app/src/main/java/com/example/helpsync/helprequest_cloudmessage/HelpRequestMessageReceiver.kt
@@ -4,8 +4,15 @@ import android.util.Log
 import com.example.helpsync.repository.CloudMessageRepository
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+
+private val serviceJob = SupervisorJob()
+private val serviceScope = CoroutineScope(Dispatchers.IO + serviceJob)
 
 class HelpRequestMessageReceiver : FirebaseMessagingService(), KoinComponent{
     private val repository: CloudMessageRepository by inject()
@@ -17,6 +24,16 @@ class HelpRequestMessageReceiver : FirebaseMessagingService(), KoinComponent{
             Log.d("FCM_Service", "received data: $receivedData")
 
             repository.postCloudMessage(receivedData)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        serviceScope.launch {
+            try {
+                repository.callRenewDeviceToken(token = token)
+            } catch (e: Exception) {
+                Log.e("debug", "Failed to send FCM token")
+            }
         }
     }
 }
