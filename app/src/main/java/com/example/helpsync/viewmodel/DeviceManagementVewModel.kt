d78320bce07c042dce363ae44fe9fb0fc1011bde
haru210 <harun20070320@gmail.com>
2025-10-27 13:25:51 +0900

    feat: callRegisterNewDeviceの作成

---
+++ app/src/main/java/com/example/helpsync/viewmodel/DeviceManagementVewModel.kt
@@ -0,0 +1,47 @@
+package com.example.helpsync.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.helpsync.repository.CloudMessageRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class DeviceManagementVewModel(
+    private val cloudMessageRepository: CloudMessageRepository
+) : ViewModel(){
+
+    fun callRegisterNewDevice(latitude: Double, longitude: Double) {
+        viewModelScope.launch{
+            val deviceId = try {
+                cloudMessageRepository.getDeviceId()
+            } catch(e: Exception) {
+                Log.d("Error", "deviceIdの取得に失敗しました")
+            }
+            try {
+                val functions = Firebase.functions("asia-northeast2")
+                val owner = FirebaseAuth.getInstance().currentUser
+                val ownerId = owner?.uid
+                val deviceToken = FirebaseMessaging.getInstance().token.await()
+                val locationMap = hashMapOf(
+                    "latitude" to latitude,
+                    "longitude" to longitude
+                )
+                val data = hashMapOf(
+                    "ownerId" to ownerId,
+                    "deviceToken" to deviceToken,
+
+                    "location" to locationMap
+                )
+            } catch(e: Exception){
+                Log.d("Error", "デバイスの登録に失敗しました")
+                Log.d("Error", "Error message: ${e.message}")
+            }
+        }
+
+    }
+}
